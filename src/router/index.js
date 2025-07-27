e906ac4bd996e1f981774f8cc00ffafb2ec7ef6f
GaziWani1 <gaziwani123@gmail.com.com>
2025-07-27 20:29:08 +0530

    router links

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter , createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import Jobs from "../views/Jobs.vue";
+import NotFoundView from "../views/NotFoundView.vue";
 import JobView from "../views/JobView.vue";
 
 const router = createRouter({
@@ -13,8 +15,19 @@ const router = createRouter({
         {
             path:'/jobs',
             name : 'jobs',
+            component : Jobs
+
+        },
+         {
+            path:'/job/:id',
+            name : 'job',
             component : JobView
 
+        },
+        {
+            path : '/:catchAll(.*)',
+            name : 'not-found',
+            component : NotFoundView
         }
     ]
 });
